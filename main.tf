22dd9eba84d6c10bb05ca2e2801c97890ffbaae3
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-03-19 12:16:26 +0530

    vpc

--- main.tf
+++ main.tf
@@ -5,8 +5,11 @@ resource "aws_vpc" "main" {
         tags = local.vpc_final_tags
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id  # vpc association id
 
-
+  tags = local.igw_final_tags
+}
 
 
 
